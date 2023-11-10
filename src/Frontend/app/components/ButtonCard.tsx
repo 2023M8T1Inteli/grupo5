ae3526895b1a6e9b71e268267bdf858ee2b10573
Thomas Brand <thomas.brand@sou.inteli.edu.br>
2023-11-10 12:48:44 -0300

    tela inicial

---
+++ src/Frontend/app/components/ButtonCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+interface ButtonCardProps {
+  icon: any;
+  text: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+const ButtonCard: React.FC<ButtonCardProps> = ({icon, text, onClick, className, disabled = false }) => {
+  return (
+    <div className='flex content-center'>
+        <button onClick={onClick} className={'w-[10rem] h-[10rem] bg-[#E7343F] rounded-xl mt-6 mr-8 text-1xl text-white font-normal' + ' ' + className} disabled={disabled}>
+            <div className='flex flex-col justify-center items-center'>
+                <Image src={icon} alt='icon'></Image> 
+                {text}
+            </div>
+            
+        </button>
+    </div>
+  );
+};
+
+ButtonCard.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+export default ButtonCard;
