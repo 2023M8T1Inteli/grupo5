e180ee05e9c8d55531e01efa41fa3e60958443f4
Elias Biondo <contato@eliasbiondo.com>
2023-11-12 16:46:32 -0300

    refactor(frontend): ide

---
+++ src/Frontend/app/helpers/codeGenerator.tsx
@@ -0,0 +1,28 @@
+import { IDroppedItem } from "../hooks/useDrop";
+
+export const commandNameMapping = (activeQuadrant : number) => ({
+    'Se': 'se ',
+    'Apertar': `quadrante == ${activeQuadrant} entao inicio\n`,
+    'Mostrar imagem': `mostrar()`,
+    'E': `\n`,
+    'Tocar som': `tocar()`,
+    'E fim': `\nfim\n`
+});
+
+export const generateCommandCode = (droppedItems : IDroppedItem[], commandMapping : { [key: string]: string }) => {
+    return droppedItems.reduce((code, item) => {
+        return code + (commandMapping[item.commandName] || '');
+    }, '');
+}
+
+export const generateRawCode = (therapyName : string, droppedItems : IDroppedItem[], commandMapping : { [key: string]: string }) => {
+    let code = `programa "${therapyName}":\n`;
+    code += 'inicio\n';
+    code += 'quadrante = ler()\n';
+    code += generateCommandCode(droppedItems, commandMapping);
+    code += 'fim.\n';
+    if (droppedItems.some(e => e.commandName === 'E fim')) {
+        console.log(code);
+    }
+    return code;
+}
