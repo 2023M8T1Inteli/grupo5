dc06587751caf5c40bf77a4634f53bac39c1ae00
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2023-11-12 19:56:21 -0300

    feat(CareApi): CodeQAL endpoint

---
+++ src/CareApi/Controllers/CodeQALController.cs
@@ -0,0 +1,29 @@
+﻿using CareApi.Services;
+using CareApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeQALController : ControllerBase
+    {
+        private readonly CodeQALService _codeQALService;
+
+        public CodeQALController(CodeQALService codeQALService)
+        {
+            _codeQALService = codeQALService;
+        }
+
+        [HttpGet]
+        public async Task<List<CodeQAL>> GetAll() =>
+            await _codeQALService.GetAllAsync();
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CodeQAL codeQal)
+        {
+            await _codeQALService.CreateAsync(codeQal);
+            return CreatedAtAction(nameof(GetAll), new { code = codeQal.Code }, codeQal);
+        }
+    }
+}
