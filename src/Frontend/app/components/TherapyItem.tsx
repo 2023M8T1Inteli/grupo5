cd1c7a86b902803f464a47c92b2604cc0d22955e
Elias Biondo <contato@eliasbiondo.com>
2023-11-12 15:16:03 -0300

    refactor(frontend): therapies page

---
+++ src/Frontend/app/components/TherapyItem.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import TableItem from './TableItem';
+import User from './User';
+import Trash from '@/public/trash.svg'
+import Pen from '@/public/pen.svg'
+import Play from '@/public/play.svg'
+import { ITherapy } from '../dashboard/therapies/page';
+
+export function TherapyItem({therapy} : { therapy: ITherapy }) {
+    return (
+        <div className='bg-white w-full p-6 flex gap-6 hover:bg-[#EAECF0] border-solid border-[#EAECF0]'>
+            <TableItem className='w-64'>{therapy.name}</TableItem>
+            <TableItem className='w-44'>{therapy.date}</TableItem>
+            <TableItem className='w-44'>
+                <User name={therapy.createdBy.name} username={therapy.createdBy.username}/>
+            </TableItem>
+            <TableItem className='w-44'>{therapy.lastExecution}</TableItem>
+            <TableItem className='w-40'>{therapy.executionCount}</TableItem>
+            <TableItem className='w-52'>
+                <User name={therapy.lastPatient.name} username={therapy.lastPatient.username}/>
+            </TableItem>
+            <TableItem className='w-40 flex justify-end'>
+                <a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Trash} alt='Excluir' /></a>
+                <a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Pen} alt='Editar' /></a>
+                <a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Play} alt='Executar' /></a>
+            </TableItem>
+        </div>
+    );
+}
--- src/Frontend/app/dashboard/therapies/page.tsx
+++ src/Frontend/app/dashboard/therapies/page.tsx
@@ -1,74 +1,69 @@
-'use client'
-import ButtonMin from '@/app/components/ButtonMin';
-import Heading from '@/app/components/Heading';
-import Subheading from '@/app/components/Subheading';
-import TableHeadItem from '@/app/components/TableHeadItem';
-import TableItem from '@/app/components/TableItem';
-import Trash from '@/public/trash.svg'
-import Pen from '@/public/pen.svg'
-import Play from '@/public/play.svg'
-import Image from 'next/image';
-import User from '@/app/components/User';
+import ButtonMin from "@/app/components/ButtonMin";
+import Heading from "@/app/components/Heading";
+import Subheading from "@/app/components/Subheading";
+import Table from "@/app/components/Table";
+import { TherapyItem } from "@/app/components/TherapyItem";
+
+export interface ITherapy {
+	name: string;
+	date: string;
+	createdBy: {
+		name: string;
+		username: string;
+	};
+	lastExecution: string;
+	executionCount: number;
+	lastPatient: {
+		name: string;
+		username: string;
+	};
+} 
 
 export default function Therapies() {
-	return(
-		<div className='flex flex-col p-16 w-full gap-16'>
-			<div className='flex justify-between items-center'>
-				<div className='flex flex-col gap-2'>
-					<Heading>Terapias</Heading>
-					<Subheading>Gerencie as terapias disponíveis</Subheading>
-				</div>
-				<div className='w-48'>
-					<ButtonMin text='Criar terapia'/>	
-				</div>
-			</div>
+    const therapies : ITherapy[] = [
+        {
+            name: 'Terapia 1',
+            date: '10/10/2021',
+            createdBy: {
+                name: 'Ana Carolina',
+                username: 'anacarolina'
+            },
+            lastExecution: '10/10/2021',
+            executionCount: 10,
+            lastPatient: {
+                name: 'Maria Luiza',
+                username: 'marialuiza'
+            }
+        },
+    ];
 
-			<div className='rounded-lg border-solid border-2 border-[#EAECF0] overflow-hidden'>
-				<div className='bg-[#F9FAFB] w-full px-6 py-3 flex gap-6'>
-					<TableHeadItem className='w-64'>Nome</TableHeadItem>
-					<TableHeadItem className='w-44'>Data de criação</TableHeadItem>
-					<TableHeadItem className='w-44'>Criado por</TableHeadItem>
-					<TableHeadItem className='w-44'>Última execução</TableHeadItem>
-					<TableHeadItem className='w-40'>Nº de execuções</TableHeadItem>
-					<TableHeadItem className='w-52'>Último paciente a executar</TableHeadItem>
-				</div>
-				<div className='bg-white w-full p-6 flex gap-6 hover:bg-[#EAECF0] border-solid border-b border-[#EAECF0]'>
-					<TableItem className='w-64'>Terapia 1</TableItem>
-					<TableItem className='w-44'>10/10/2021</TableItem>
-					<TableItem className='w-44'>
-						<User name='Ana Carolina' username='anacarolina'/>
-					</TableItem>
-					<TableItem className='w-44'>10/10/2021</TableItem>
-					<TableItem className='w-40'>10</TableItem>
-					<TableItem className='w-52'>
-						<User name='Maria Luiza' username='marialuiza'/>
-					</TableItem>
-					<TableItem className='w-40 flex justify-end'>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Trash} alt='Excluir' /></a>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Pen} alt='Editar' /></a>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300' href='/dashboard/therapies/dec7a9bd-e265-4100-a8e2-3cd9a881af3c'><Image src={Play} alt='Executar' /></a>
-					</TableItem>
-				</div>
-				<div className='bg-white w-full p-6 flex gap-6 hover:bg-[#EAECF0] border-solid border-[#EAECF0]'>
-					<TableItem className='w-64'>Terapia 2</TableItem>
-					<TableItem className='w-44'>22/03/2023</TableItem>
-					<TableItem className='w-44'>
-						<User name='Carol Braga' username='carolbraga'/>
-					</TableItem>
-					<TableItem className='w-44'>05/11/2023</TableItem>
-					<TableItem className='w-40'>344</TableItem>
-					<TableItem className='w-52'>
-						<User name='João Pedro' username='joaopedro'/>
-					</TableItem>
-					<TableItem className='w-40 flex justify-end'>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Trash} alt='Excluir' /></a>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Pen} alt='Editar' /></a>
-						<a className='cursor-pointer w-10 h-10 flex justify-center items-center hover:scale-125 duration-300'><Image src={Play} alt='Executar' /></a>
-					</TableItem>
-				</div>
-			</div>
+    const headers = [
+		{name: 'Nome', spacing: '64'}, 
+		{name: 'Data de criação', spacing: '44'},
+		{name: 'Criado por', spacing: '44'},
+		{name: 'Última execução', spacing: '44'},
+		{name: 'Nº de execuções', spacing: '40'},
+		{name: 'Último paciente a executar', spacing: '52'},
+		{name: '', spacing: '40'}
+	];
 
+    return (
+        <div className='flex flex-col p-16 w-[85%] gap-16'>
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col gap-2'>
+                    <Heading>Terapias</Heading>
+                    <Subheading>Gerencie as terapias disponíveis</Subheading>
+                </div>
+                <div className='w-48'>
+                    <ButtonMin text='Criar terapia'/>	
+                </div>
+            </div>
 
-		</div>
-	)
+            <Table headers={headers}>
+                {therapies.map((therapy, index) => (
+                    <TherapyItem key={index} therapy={therapy} />
+                ))}
+            </Table>
+        </div>
+    );
 }
