4af4214b8d370d8e88106ecee3fe2e917efd33c9
Munhozs <pedro.rivero@sou.inteli.edu.br>
2023-11-14 16:23:21 -0300

    chore(CareApi): remove CodeQALCollectionName
    Small adjustment

--- src/CareApi/Models/CareApiDBSettings.cs
+++ src/CareApi/Models/CareApiDBSettings.cs
@@ -7,6 +7,6 @@ namespace CareApi.Models
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
         public string UserCollectionName { get; set; } = null!;
-        public string CodeQALCollectionName { get; set; } = null!;
+        public string TherapyCollectionName { get; set; } = null!;
     }
 }
