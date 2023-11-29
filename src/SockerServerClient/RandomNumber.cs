fc86f6ade24439b6d1725d9ff3ac81cc9cb4479e
Munhozs <pedro.rivero@sou.inteli.edu.br>
2023-11-29 17:02:18 -0300

    chore(SockerServerClient): comments

--- src/SockerServerClient/RandomNumber.cs
+++ src/SockerServerClient/RandomNumber.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Threading.Tasks;
 
-
-//SUBSTITUIR ESSA FUNÇÃO PELA QUE LÊ OS VALORES DO GREG
+//CHANGE THIS CLASS FOR THE GREG MAKER OUTPUT
 public static class RandomNumber
 {
+    // Method to generate a random number between 1 and 12
     public static int Generate()
     {
+        // Creating a new instance of the Random class
         Random random = new Random();
+
+        // Generating and returning a random number between 1 and 12
         return random.Next(1, 13);
     }
 }
