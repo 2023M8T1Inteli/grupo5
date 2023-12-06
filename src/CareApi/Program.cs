a7aa1817b677a48765b5716cbd6ca4c198c88a05
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2023-12-05 23:30:16 -0300

    feat: authentication

--- src/CareApi/Program.cs
+++ src/CareApi/Program.cs
@@ -1,7 +1,15 @@
 using CareApi.Models;
 using CareApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 
 builder.Services.AddCors( options =>
 {
@@ -11,6 +19,29 @@ builder.Services.AddCors( options =>
             .AllowAnyMethod()
     );
 });
+
+// Add authentication
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+}
+);
+
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 builder.Services.Configure<CareApiDBSettings>(builder.Configuration.GetSection("CareApiDB"));
@@ -35,6 +66,9 @@ if (app.Environment.IsDevelopment())
 app.UseCors();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
